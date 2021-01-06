7a7f8f997b929cd8e7240cf1ee8516b51f1e9282
Abdul Motaleb (Munna) <71353762+munna4@users.noreply.github.com>
2021-01-07 00:50:46 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+ line-bar = document.querySelector('.line-bar')
+ navbar = document.querySelector('.navbar')
+ navlist = document.querySelector('.navlist')
+ rightnav = document.querySelector('.rightnav')
+
+
+ line-bar.addEventListener('click', () => {
+     navlist.classList.toggle('v-class');
+     right.classList.toggle('v-class');
+     navbar.classList.toggle('h-navbar');
+
+ })
+ 
